7a80a1ef483fafc6a8e93131d58b40a4a39a36f2
mrtanis <mrtanis@appledemacbook-pro.local>
2018-09-26 14:52:35 +0800

    Initial Commit

---
+++ AppStoreAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreAnimation
+//
+//  Created by mrtanis on 2018/9/26.
+//  Copyright © 2018 mrtanis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
