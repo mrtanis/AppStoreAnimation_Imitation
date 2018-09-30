30a0364b9835d696dfe95702f21f3f308a7d4945
mrtanis <mrtanis1992@hotmail.com>
2018-09-30 23:49:15 +0800

    uu

---
+++ AppStoreAnimation/Section/Today/View/Cell/DetailCell/TodayCardDetailCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodayCardDetailCell.swift
+//  AppStoreAnimation
+//
+//  Created by mrtanis on 2018/9/30.
+//  Copyright © 2018 mrtanis. All rights reserved.
+//
+
+import UIKit
+
+class TodayCardDetailCell: UITableViewCell, Reusable {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
